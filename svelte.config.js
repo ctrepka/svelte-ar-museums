fb6232347e2dab7fd80619f6f881bfcdce8fef8e
ctrepka <chris.repka@twdb.texas.gov>
2023-11-14 13:33:55 -0600

    prep static

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: "200.html"
+			fallback: "app.html"
 		}),
 		paths: {
             base: process.env.NODE_ENV === 'production' ? '/svelte-ar-museums' : '',
